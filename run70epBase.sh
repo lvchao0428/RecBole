ace3dc3cb90cd7027dac2c77bb1cf4c71e2825ac
ultrapp <410148119@qq.com>
2025-11-22 19:57:08 +0800

    update 1122 exp ,delete useless config for sasrec base

--- run70epBase.sh
+++ run70epBase.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-# Note: Even though this is ID-only baseline, we still include the stability config
-# to ensure consistent model initialization (item_emb_norm will exist but not be used
-# since disable_text_feature=true prevents fusion)
 python run_recbole.py \
-  --model SASRec_Align \
+  --model SASRec \
   --dataset Amazon_Beauty \
-  --config_files "sasrec_align_qwen3.yaml overrides/id_only_70ep.yaml overrides/stability_enhance_with_id_ln.yaml"
+  --config_files "sasrec_baseline_70ep.yaml"
--- two_phase_run_tfidf_llm.sh
+++ two_phase_run_tfidf_llm.sh
@@ -16,7 +16,7 @@ python scripts/two_phase_train.py \
   --dataset Amazon_Beauty \
   --config_files "sasrec_align_qwen3.yaml" \
   --phase_a_grid \
-  --align_grid "0.05" \
+  --align_grid "0.05,0.1,0.2" \
   --tau_grid "0.05,0.07" \
   --backbone_burnin_epochs 10 \
   --burnin_eval_step 2 \
@@ -30,7 +30,7 @@ python scripts/two_phase_train.py \
   --phase_a_text_gate_reg_l2 0.05 \
   --phase_b_alignment_weight 0.01 \
   --phase_b_text_gate_reg_l2 0.02 \
-  --phase_b_text_weight 0.6 \
+  --phase_b_text_weight 0.5 \
   --phase_a_auto_to_b \
   --phase_b_epochs 40 \
   --backbone_lr_scale 0.1 \
