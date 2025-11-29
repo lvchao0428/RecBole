1ab4ecd21b44171758bcf92b0130dbf96531d86f
ultrapp <410148119@qq.com>
2025-11-29 23:07:09 +0800

    update yelp config

--- two_phase_run_tfidf_llm_yelp.sh
+++ two_phase_run_tfidf_llm_yelp.sh
@@ -9,22 +9,22 @@ python scripts/two_phase_train.py \
   --dataset yelp \
   --config_files "yelp_config/sasrec_align_yelp_qwen3.yaml" \
   --phase_a_grid \
-  --align_grid "0.02,0.05,0.1" \
-  --tau_grid "0.03,0.05,0.07" \
+  --align_grid "0.05" \
+  --tau_grid "0.05,0.07" \
   --backbone_burnin_epochs 10 \
   --burnin_eval_step 2 \
   --phase_a_epochs 10 \
   --phase_a_eval_step 2 \
   --phase_a_valid_metric NDCG@10 \
-  --ndcg_baseline 0.0157 \
-  --ndcg_gain_threshold 0.005 \
-  --lr_text_head 5e-4 \
-  --lr_dnn_cross 3e-4 \
-  --phase_a_text_gate_reg_l2 0.02 \
-  --phase_b_alignment_weight 0.03 \
+  --ndcg_baseline 0.0425 \
+  --ndcg_gain_threshold 0.01 \
+  --lr_text_head 1e-3 \
+  --lr_dnn_cross 5e-4 \
+  --phase_a_text_gate_reg_l2 0.05 \
+  --phase_b_alignment_weight 0.05 \
   --phase_a_auto_to_b \
-  --phase_b_epochs 35 \
-  --backbone_lr_scale 0.2 \
+  --phase_b_epochs 15 \
+  --backbone_lr_scale 0.1 \
   --checkpoint_dir ./saved/phase_runs \
   --seed 2025 \
   --variant_features "sasrec,tfidf,llm,add epoch" \
