9239be4929839a652639cab43c3fe1d534ec0b25
ultrapp <410148119@qq.com>
2026-01-03 11:50:42 +0800

    update 0103 train config

--- two_phase_run_tfidf_toys_stratified.sh
+++ two_phase_run_tfidf_toys_stratified.sh
@@ -28,8 +28,8 @@ python scripts/two_phase_train.py \
   --dataset Amazon_Toys_and_Games \
   --config_files "sasrec_align_toys_base_stratified.yaml" \
   --phase_a_grid \
-  --align_grid "0.05" \
-  --tau_grid "0.07" \
+  --align_grid "0.08" \
+  --tau_grid "0.12" \
   --backbone_burnin_epochs 10 \
   --burnin_eval_step 2 \
   --phase_a_epochs 20 \
@@ -37,7 +37,7 @@ python scripts/two_phase_train.py \
   --phase_a_valid_metric "MRR@10" \
   --metric_baseline 0.0272 \
   --metric_gain_threshold 0.01 \
-  --lr_text_head 1e-3 \
+  --lr_text_head 2e-3 \
   --lr_dnn_cross 5e-4 \
   --phase_a_text_gate_reg_l2 0.05 \
   --phase_b_alignment_weight 0.05 \
