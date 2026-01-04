f04bca49786e0998a1d08b0509a204b06f065e2d
charlie <410148119@qq.com>
2026-01-04 10:14:53 +0800

    update beauty train ablation config

--- beauty_train/two_phase_run_multiview_v2_stratified.sh
+++ beauty_train/two_phase_run_multiview_v2_stratified.sh
@@ -38,21 +38,21 @@ python ${base_dir}/scripts/two_phase_train.py \
   --dataset Amazon_Beauty \
   --config_files "${yaml_dir}/sasrec_align_multi_view_v2_stratified.yaml" \
   --phase_a_grid \
-  --align_grid "0.03" \
-  --tau_grid "0.1" \
+  --align_grid "0.08" \
+  --tau_grid "0.05" \
   --backbone_burnin_epochs 10 \
   --burnin_eval_step 2 \
-  --phase_a_epochs 15 \
+  --phase_a_epochs 20 \
   --phase_a_eval_step 1 \
   --phase_a_valid_metric "MRR@10" \
   --metric_baseline 0.0272 \
   --metric_gain_threshold 0.01 \
-  --lr_text_head 1e-3 \
+  --lr_text_head 2e-3 \
   --lr_dnn_cross 5e-4 \
-  --phase_a_text_gate_reg_l2 0.05 \
-  --phase_b_alignment_weight 0.05 \
-  --phase_b_text_gate_reg_l2 0.05 \
-  --phase_b_text_weight 0.8 \
+  --phase_a_text_gate_reg_l2 0.01 \
+  --phase_b_alignment_weight 0.15 \
+  --phase_b_text_gate_reg_l2 0.03 \
+  --phase_b_text_weight 1.0 \
   --phase_a_auto_to_b \
   --phase_b_epochs 40 \
   --backbone_lr_scale 0.1 \
